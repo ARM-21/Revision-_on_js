64bc06f9366a999e71b123a05074217f9ed12284
ARM-21 <manojneupane787@gmail.com>
2024-06-18 23:32:05 +0545

    append appendChild node Element

---
+++ Day-8,dom all concepts/appendVsappendChild and cloneNode_to_copy.js
@@ -0,0 +1,18 @@
+
+//append --->adding something after something
+//append() method is used to insert a set of Node objects or DOMString objects after the last child of the ParentNode.
+//It is not supported by Internet Explorer.
+//It is supported by all other major browsers.
+
+
+//appendChild() method in js
+//appendChild() method is used to insert a Node object as the last child of the ParentNode.
+//It is supported by all major browsers.
+//It returns the appended Node object.
+//It throws a DOMException if the Node object is already part of the DOM tree.
+
+
+// we can append a string using append whereas cannot use appendChild to append string
+// we can append a node using appendChild whereas cannot use append to append node
+//append() --- return type node object
+//appendChild -- return undefined
---
+++ Day-8,dom all concepts/nodeVsElement.js
@@ -0,0 +1,17 @@
+
+//nodes in js
+// 1. element
+// 2. attribute
+// 3. text
+// 4. comment
+// 5. document
+
+//what is node in js
+// 1. every element, attribute, text in html is a node
+// 2. document itself is a node
+
+//what is element in js
+// 1. every tag in html is an element
+// 2. element is a node
+// 3. element is a part of the document
+
