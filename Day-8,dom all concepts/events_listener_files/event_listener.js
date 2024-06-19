ac789cc7a7d1d97cc1eea55e4245092c8bec4aab
ARM-21 <manojneupane787@gmail.com>
2024-06-19 22:46:19 +0545

    event listener finished and form and event object start

--- Day-8,dom all concepts/events_listener_files/event_listener.js
+++ Day-8,dom all concepts/events_listener_files/event_listener.js
@@ -1,10 +1,11 @@
-const button = document.querySelector(".container .add-card");
+const button = document.querySelector(".add-card");
 const container = document.querySelector(".container")
 let count =1;
 button.addEventListener('click',(e)=>{
 let newCard = document.createElement("div");
 newCard.classList.add('card');
 newCard.innerText = count++;
+container.append(newCard);
 
 
 })
