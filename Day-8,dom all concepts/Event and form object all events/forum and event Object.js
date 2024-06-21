103cc5c279622d5bf1020e2dbcdba8f6180442aa
ARM-21 <manojneupane787@gmail.com>
2024-06-21 22:30:24 +0545

    form object and event object -----> keyboard event started

---
+++ Day-8,dom all concepts/Event and form object all events/forum and event Object.js
@@ -0,0 +1,30 @@
+
+
+//important events in js
+//1. onload event
+// 1. onclick ---> click
+// 2. ondblclick ---> dblclick
+// 3. onmouseover ---> mouseover
+// 4. onmouseout ---> mouseout
+// 5. onmousedown ---> mousedown
+// 6. onmouseup ---> mouseup
+// 7. onmousemove ---> mousemove
+// 8. onkeydown ---> keydown
+
+//9.onfocus --> focus ---> captures value when focused in field
+//10.onblur --> blur ----> captures value when focus is removed
+//11.onchange --> change ----> when field is clicked and click outside once captures value
+//12.onsubmit --> submit 
+//13.onreset --> reset
+//14.onselect --> select
+//15.oninput --> input ---> fires when in each character input
+
+
+
+
+
+//<--------------------------------------------------------------------------------------------------------->
+/** default behavior of button is type = "submit" */
+
+// currentTarget---->shows event triggered in Element in
+// e.target -----> got changed every time?
